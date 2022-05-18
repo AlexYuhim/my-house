6af87f7ed5d7d65245d29fb03c0f800878e07910
AlexYuhim <88594227+AlexYuhim@users.noreply.github.com>
2022-05-18 16:43:50 +0300

    feat(services): реализация сервисной  функции bem.js (#5)
    * feat(services): реализация сервисной  функции bem.js
    
    * fixed(services): изменение нейминга аргументов
    
    * fixd(services): рефакторинг кода
    
    * fixed(services): рефакторинг кода, вынес получение данных из объекта в отдельную функцию
    
    * fixed(cervices): рефакторинг кода: удалил лишний if
    
    * fixed(cervices): удалил рудимент, перенес функцию

---
+++ src/services/bem.js
@@ -0,0 +1,48 @@
+function bem(block) {
+  function valueModFromObj(obj, element) {
+    let valueFromObj = '';
+
+    Object.keys(obj).forEach((key) => {
+      if (obj[key]) {
+        valueFromObj += ' ' + block + element + '_' + key;
+      }
+
+      if (typeof obj[key] === 'string') {
+        valueFromObj += '_' + obj[key];
+      }
+    });
+
+    return valueFromObj;
+  }
+
+  return function (elementOrMod, modif) {
+    let element = '';
+    let mod = '';
+
+    if (typeof elementOrMod === 'string') {
+      element = '__' + elementOrMod;
+    }
+
+    if (typeof elementOrMod === 'object') {
+      mod = valueModFromObj(elementOrMod, '');
+    }
+
+    if (typeof elementOrMod === 'string' && typeof modif === 'object') {
+      element = '__' + elementOrMod;
+
+      mod = valueModFromObj(modif, element);
+    }
+
+    return block + element + mod;
+  };
+}
+
+const block = bem('block');
+
+console.log(block()); // block
+console.log(block({ main: true, size: 'view' }));
+console.log(block({ modA: true, modB: true, modC: 'value' })); // block block_modA block_modC_value
+console.log(block('element')); // block__element
+console.log(block('element', { modA: true })); // block__element block__element_modA
+console.log(block('element', { modA: false, modB: 'value' })); // block__element block__element_modB_value
+console.log(block('element', { modA: false, modB: false })); // block__element
