33b47c445c8b4e3fe0aad793dadb057d6580db80
Alexander Yuhimenko <a.yuhim@mail.ru>
2022-07-01 18:47:09 +0300

    feat(components):Создание компонета Select

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import '../src/styles/main.css';
 
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import MultiSelect from './components/Select/Select';
 
 function App() {
   return (
     <div className='App'>
       <Button>Привет</Button>
       <Input />
+      <MultiSelect />
     </div>
   );
 }
